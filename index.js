282e8855b2f39a6416437c0eb6e52b7aefc93d59
Evan K <EvanK311@gmail.com>
2020-10-02 20:58:37 -0700

    good working version

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 // install Inquirer
 const inquirer = require("inquirer");
 const fs = require("fs");
-const util = require("util");
+// const util = require("util");
 
 // require all the depencies I'll need - inquirer, fs
-const writeFileAsync = util.promisify(fs.writeFile)
+// const writeFileAsync = util.promisify(fs.writeFile)
 
 // create array of questions to prompt user
 function readmePrompt() {
-    inquirer.prompt([
-        {
-            type: "input",
-            message: "Enter the project title.",
-            name: "title"
-        },
-        {
+inquirer.prompt([
+    {
+         type: "input",
+        message: "Enter the project title.",
+        name: "title"
+    },
+    {
             type: "input",
             message: "Describe the project",
             name: "description"
@@ -25,7 +25,7 @@ function readmePrompt() {
             name: "installs"
         },
         {
-            type: "input",
+            type: "list",
             message: "What licenses were used",
             choices:
                 ["MIT",
@@ -56,58 +56,63 @@ function readmePrompt() {
             name: "questions"
         }
 
-    ]).then(function (answer) {
-        // ReadMe format template begins here
-        const readMeTemplate =
-            `# ${answer.title}
+]).then((answer) => {
+            // ReadMe format template begins here
+const readMeTemplate =
+`# ${answer.title}
             
-            ##Contents
+##Contents
             
-            1.[Introduction] (#description)
+1.[Introduction](#description)
                
-            2.[Installation] (#install)
+2.[Installation](#install)
             
-            3.[Licenses] (#license)
+3.[Licenses](#license)
             
-            4.[Contributions] (#contribution)
+4.[Contributions](#contribution)
             
-            5.[Contact] (#contact)
+5.[Contact](#contact)
             
-            6.[Github] (#github)
+6.[Github](#github)
             
-            7.[Additional Info] (#questions)
+7.[Additional Info](#questions)
             
             
-            <br>
+<br>
              
-            ## 1. Introduction {#description}
-            ${answer.description}
-            ## 2. Installation {#install}
-            ${answer.installs}
-            ## 3. Licenses {#license}
-            ${answer.licenses}
-            ## 4. Contributions {#contribution}
-            ${answer.contributions}
-            ## 5. Contact {#contact}
-            ${answer.contact}
-            ## 6. Github {#github}
-            ${answer.Github}
-            ## 7. Additional Info {#questions}
-            ${answer.questions}`
+## 1. Introduction {#description}
+${answer.description}
 
-        // variable for creation of new readme file
-        const fileName = `${answer.title.replace(/\s/g, '').toLowerCase()}.md`;
+## 2. Installation {#install}
+${answer.installs}
 
-        fs.writeFile(fileName, readmePrompt, function (err) {
-            if (err) {
-                return console.log(err)
-            }
-            console.log("All set!")
-        })
+## 3. Licenses {#license}
+${answer.licenses}
+
+## 4. Contributions {#contribution}
+${answer.contributions}
+
+## 5. Contact {#contact}
+${answer.contact}
+
+## 6. Github {#github}
+${answer.Github}
+
+## 7. Additional Info {#questions}
+${answer.questions}`;
+
+            // variable for creation of new readme file
+const fileName = `${answer.title.replace(/\s/g, '').toLowerCase()}.md`;
+
+fs.writeFile(fileName, readMeTemplate, function (err) {
+    if (err) {
+    return console.log(err);
+                }
+                console.log("All set!");
+        });
 
 
     })
-
 
 }
 
