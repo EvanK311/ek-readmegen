fbb6fa1fd5fcb1f04c3d495b85c235c93535818b
Evan K <EvanK311@gmail.com>
2020-10-02 20:07:58 -0700

    readme process and skeleton almost complete

--- index.js
+++ index.js
@@ -1,64 +1,139 @@
 // install Inquirer
 const inquirer = require("inquirer");
 const fs = require("fs");
+const util = require("util");
+
 // require all the depencies I'll need - inquirer, fs
+const writeFileAsync = util.promisify(fs.writeFile)
 
 // create array of questions to prompt user
-function generateHTML(ansData) { }
+function readmePrompt() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter the project title.",
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "Describe the project",
+            name: "description"
+        },
+        {
+            type: "input",
+            message: "What nodes to install?",
+            name: "installs"
+        },
+        {
+            type: "input",
+            message: "What licenses were used",
+            choices:
+                ["MIT",
+                    "IBM",
+                    "Eclipse",
+                    "Apache",
+                    "GNU"],
+            name: "licenses"
+        },
+        {
+            type: "input",
+            message: "any contributors?",
+            name: "contributions"
+        },
+        {
+            type: "input",
+            message: "Preferred contact E-mail for questions",
+            name: "contact"
+        },
+        {
+            type: "input",
+            message: "enter GitHub URL",
+            name: "Github"
+        },
+        {
+            type: "input",
+            message: "Additional info about user contacting",
+            name: "questions"
+        }
 
-inquirer.prompt([
-    {
-        type: "input",
-        message: "Enter the project title.",
-        name: "title"
-    },
-    {
-        type: "input",
-        message: "Describe the project",
-        name: "description"
-    },
-    {
-        type: "input",
-        message: "Table of Contents",
-        name: "contents"
-    },
-    {
-        type: "input",
-        message: "What nodes to install?",
-        name: "installs"
-    },
-    {
-        type: "input",
-        message: "enter GitHub URL",
-        name: "usage"
-    },
-    {
-        type: "input",
-        message: "enter GitHub URL",
-        name: "license"
-    },
-    {
-        type: "input",
-        message: "enter GitHub URL",
-        name: "contributing"
-    }, {
-        type: "input",
-        message: "enter GitHub URL",
-        name: "Tests"
-    }, {
-        type: "input",
-        message: "enter GitHub URL",
-        name: "questions"
-    },
+    ]).then(function createMD(answer) {
+        // ReadMe format template begins here
+        const readMeTemplate =
+            `# ${answer.title}
+            
+            ##Contents
+            
+            1.[Introduction] (#description)
+               
+            2.[Installation] (#install)
+            
+            3.[Licenses] (#license)
+            
+            4.[Contributions] (#contribution)
+            
+            5.[Contact] (#contact)
+            
+            6.[Github] (#github)
+            
+            7.[Additional Info] (#questions)
+            
+            
+            <br>
+             
+            ## 1. Introduction {#description}
+            ${answer.description}
+            ## 2. Installation {#install}
+            ${answer.installs}
+            ## 3. Licenses {#license}
+            ${answer.licenses}
+            ## 4. Contributions {#contribution}
+            ${answer.contributions}
+            ## 5. Contact {#contact}
+            ${answer.contact}
+            ## 6. Github {#github}
+            ${answer.Github}
+            ## 7. Additional Info {#questions}
+            ${answer.questions}`
+        
+            // variable for creation of new readme file
+        const fileName = `${answers.title.replace(/\s/g, '').toLowerCase()}.md`;
 
-])
+        fs.writeFile(fileName, readmePrompt, function () {
+            if (err) {
+                return console.log(err)
+            }
+            console.log("All set!")
+        })
+                
+        
+    })
+
+
+}
+
+
+readmePrompt()
+
+
+
 // write a readME in a markdown file for a template reference
 
 // function that will generate readme template
+// readmePrompt()
+//     .then(function (answer) {
+//         const readMeMd = createMD(answer);
 
-// use inquirer to prompt users with questions. (questions in homework readme)
+//         return writeFileAsync("ReadMe.md", readMeMd);
+//     })
+//     .then(function () {
+//         console.log("Successfully wrote to index.html");
+//     })
+//     .catch(function (err) {
+//         console.log(err);
+//     });
+
 
 // user answers that inquirer returns and pass them to readME generator function
 
-// write file using template generated from readME function
+
 
